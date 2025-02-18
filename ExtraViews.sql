ec4f746d878ec159bbde5ce138725b1c27525396
Nikhil2448 <129896442+Niki24-48@users.noreply.github.com>
2025-02-18 13:47:24 +0100

    SQL

---
+++ ExtraViews.sql
@@ -0,0 +1,9 @@
+create view PeopleShortV as SELECT id, name, url FROM People;
+
+create view PlanetsShortV as SELECT id, name, url FROM planets;
+
+create view SpeciesShortV as SELECT id, name, url FROM species;
+
+create view StarshipsShortV as SELECT id, name, url FROM starships;
+
+create view VehiclesShortV as SELECT id, name, url FROM vehicles;
